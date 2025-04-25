9808dfd56fe6986ff8b1076f98ca5b1eba7c7766
Lívia Oliveira <lligiaoliveira00@gmail.com>
2025-04-25 09:44:48 -0300

    Mostre sua playlist favorita

---
+++ ex2.js
@@ -0,0 +1,6 @@
+let playlist = [ 'Honest' , 'Meio Termo' , 'Anormal' , 'Duas metades' ];
+let quantidade = playlist.length;
+
+for (let i = 0 ; i < quantidade; i++){
+    console.log (playlist [i]);
+}
